5c8f32734a4fbd648ce9e0eb591bd4b827300765
Mohit Chouhan <mohit_chouhan@icloud.com>
2022-08-29 23:27:03 +0530

    Update MinDeletionsToLongestPalindromicSubsequence.java

--- MinDeletionsToLongestPalindromicSubsequence.java
+++ MinDeletionsToLongestPalindromicSubsequence.java
@@ -12,16 +12,10 @@ public class MinDeletionsToLongestPalindromicSubsequence {
       for (int j = 0; j <= n; j++) {
         if (i == 0 || j == 0) {
           t[i][j] = 0;
-        }
-      }
-    }
-
-    for (int i = 1; i <= n; i++) {
-      for (int j = 1; j <= n; j++) {
-        if (s1.charAt(i-1) == s2.charAt(j-1)) {
-          t[i][j] = 1 + t[i - 1][j - 1];
+        } else if (s1.charAt(i-1) == s2.charAt(j-1)) {
+            t[i][j] = 1 + t[i - 1][j - 1];
         } else {
-          t[i][j] = Math.max(t[i][j - 1], t[i - 1][j]);
+            t[i][j] = Math.max(t[i][j - 1], t[i - 1][j]);
         }
       }
     }
